b72e93c0f1c255bfb8919941eb423ce0a60a3113
dan <dchestnyh21@gmail.com>
2023-06-13 07:09:49 +0300

    Added tests for profile pages.

--- app/users/tests/test_profiles.py
+++ app/users/tests/test_profiles.py
@@ -1,9 +1,11 @@
+import sys
 import pytest
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
 
 pytestmark = pytest.mark.django_db
+
 
 def test_get_profile_page_success(client):
     """Test accessing a profile page is successful."""
@@ -12,7 +14,7 @@ def test_get_profile_page_success(client):
 
     res = client.get(url)
 
-    print(res.content)
+    sys.stdout.write(str(res.content))
     assert res.status_code == 200
-
-
+    assert user.email in str(res.content)
+    assert user.password not in str(res.content)
