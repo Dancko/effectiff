8bbc6b7594fc0e631486b57ff6ec1a47af26f0ec
dan <dchestnyh21@gmail.com>
2023-06-26 19:59:05 +0300

    Added project creation page.

--- app/projects/urls.py
+++ app/projects/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('my_projects/<int:pk>/', views.myProjectsPage, name='my_projects'),
     path('<int:pk>/', views.projectPage, name='project'),
+    path('new_project/', views.createProjectPage, name='create_project'),
 ]
--- app/projects/views.py
+++ app/projects/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
 
 from core.models import Project, Task
+from .forms import ProjectCreationForm
 
 
 @login_required(login_url='login')
@@ -16,3 +17,18 @@ def projectPage(request, pk):
     project = Project.objects.get(id=pk)
     tasks = Task.objects.filter(project__id=project.id)
     return render(request, 'projects/project.html', {'project': project, 'tasks': tasks})
+
+
+@login_required(login_url='login')
+def createProjectPage(request):
+    page = 'create'
+    form = ProjectCreationForm()
+    if request.method == 'POST':
+        form = ProjectCreationForm(request.POST)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.owner = request.user
+            project.save()
+            form.save_m2m()
+            return redirect('my_projects', pk=request.user.id)
+    return render(request, 'projects/create_update_project.html', {'form': form})
