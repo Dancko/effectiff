37c3c9a417f2c9d676b271340c2061e2fdfb377a
dan <dchestnyh21@gmail.com>
2023-07-11 07:25:16 +0300

    Changed checks.yml.

--- app/app/settings.py
+++ app/app/settings.py
@@ -82,9 +82,9 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['POSTGRES_DB'],
-        'USER': os.environ['POSTGRES_USER'],
-        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASS'],
         'HOST': 'db',
         'PORT': 5432,
     }
