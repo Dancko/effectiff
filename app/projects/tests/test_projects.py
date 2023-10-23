adb45d7068c5ee3120a14e32e7a5b2335bc7120c
unknown <dchestnyh21@gmail.com>
2023-10-23 18:10:07 +0700

    Changed project tests

--- app/projects/tests/test_projects.py
+++ app/projects/tests/test_projects.py
@@ -11,14 +11,13 @@ pytestmark = pytest.mark.django_db
 
 def test_myprojects_get_success(request, client):
     """Test get request for authed user is a success."""
-    user = get_user_model().objects.create_user(email='test@example.com', password='testpass123')
-    project = Project.objects.create(
-        name='TestProj',
-        owner=user
+    user = get_user_model().objects.create_user(
+        email="test@example.com", password="testpass123"
     )
+    project = Project.objects.create(name="TestProj", owner=user)
     project.participants.add(user)
-    client.login(email=user.email, password='testpass123')
-    url = reverse('my_projects', args=[str(user.id)])
+    client.login(email=user.email, password="testpass123")
+    url = reverse("my_projects", args=[str(user.uuid)])
 
     res = client.get(url)
 
@@ -30,12 +29,11 @@ def test_myprojects_get_success(request, client):
 
 def test_my_project_unauth_redirect(client):
     """Test unauthed user is redirected to login page."""
-    user = get_user_model().objects.create_user(email='test@example.com', password='testpass123')
-    project = Project.objects.create(
-        name='TestProj',
-        owner=user
+    user = get_user_model().objects.create_user(
+        email="test@example.com", password="testpass123"
     )
-    url = reverse('my_projects', args=[str(user.id)])
+    project = Project.objects.create(name="TestProj", owner=user)
+    url = reverse("my_projects", args=[str(user.uuid)])
 
     res = client.get(url)
 
