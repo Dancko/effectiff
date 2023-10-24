1a62a1f12301c33cdda1cdd462f248571b1a6d2c
unknown <dchestnyh21@gmail.com>
2023-10-24 10:56:56 +0700

    Configured celery and redis

--- app/app/settings.py
+++ app/app/settings.py
@@ -131,6 +131,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "core.user"
 
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_BACKEND", "redis://redis:6379/0")
+
 TINYMCE_DEFAULT_CONFIG = {
     "height": "320px",
     "width": "100%",
