29cf5c3ef569b2741bb69d72d4bf3af8505cc5e0
unknown <dchestnyh21@gmail.com>
2024-01-04 10:37:14 +0700

    Added tests for getting task pages.

--- app/tasks/tests/conftest.py
+++ app/tasks/tests/conftest.py
@@ -1,6 +1,9 @@
 import pytest
+import datetime
 
 from users.models import User
+from projects.models import Project
+from tasks.models import Task
 
 # from django.contrib.auth import get_user_model
 
@@ -14,5 +17,23 @@ def user_data():
 
 @pytest.fixture
 def create_testuser(user_data):
+    """Fixture for creating a user in tests."""
+
     test_user = User.objects.create_user(**user_data)
     return test_user
+
+
+@pytest.fixture
+def create_test_task(create_testuser, user_data):
+    """Fixture for creating a test task."""
+
+    test_user = create_testuser
+    test_project = Project.objects.create(title="Test Project", owner=test_user)
+    test_task = Task.objects.create(
+        title="Test Task",
+        project=test_project,
+        deadline=datetime.datetime(2026, 10, 12, 0, 0, tzinfo=datetime.timezone.utc),
+        assigned_to=test_user,
+    )
+
+    return test_task
--- app/tasks/tests/test_tasks.py
+++ app/tasks/tests/test_tasks.py
@@ -1,9 +1,7 @@
 import pytest
 
-from django.contrib.auth import get_user_model, login, authenticate
+from django.contrib.auth import get_user_model
 from django.urls import reverse
-
-from users import views
 
 
 pytestmark = pytest.mark.django_db
@@ -11,8 +9,9 @@ pytestmark = pytest.mark.django_db
 User = get_user_model()
 
 
-def test_redirect_unathed_home_page(client):
+def test_redirect_unauthed_home_page(client):
     """Test homepage status code 302 for unauthed users."""
+
     url = reverse("my_tasks")
     res = client.get(url)
 
@@ -20,11 +19,26 @@ def test_redirect_unathed_home_page(client):
     assert res.url == reverse("login") + "?next=/"
 
 
-def test_get_homepage_auth(client, create_testuser, user_data):
-    """Test homepage status code 200 for authed users."""
+@pytest.mark.parametrize(
+    "test_url",
+    ["task_detail", "edit_task", "delete_task", "change_assignee", "change_st"],
+)
+def test_redirect_logout_user(client, create_test_task, test_url):
+    """Test redirect a logged out user from pages that require auth."""
+    test_task = create_test_task
+
+    url = reverse(test_url, args=[test_task.uuid])
+    res = client.get(url)
+
+    assert res.status_code == 302
+
+
+@pytest.mark.parametrize("test_url", ["my_tasks", "create_task"])
+def test_get_homepage_auth(client, create_testuser, test_url):
+    """Test homepage and create_task status code 200 for authed users."""
 
     create_testuser
-    url = reverse("my_tasks")
+    url = reverse(test_url)
 
     client.login(email="test@example.com", password="pass123")
     res = client.get(url)
