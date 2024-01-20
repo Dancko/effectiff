071d09ee8c3139c19f03c380d90840a3953896f2
unknown <dchestnyh21@gmail.com>
2024-01-20 21:11:24 +0700

    Added tests for task.is_outdated.

--- app/tasks/tests/test_tasks_models.py
+++ app/tasks/tests/test_tasks_models.py
@@ -1,8 +1,25 @@
 import pytest
+import datetime
 
 
-@pytest.mark.django_db
+pytestmark = pytest.mark.django_db
+
+
 def test_task_str_return(task_factory):
     task = task_factory(title="Test Task 1")
 
     assert str(task) == "Test Task 1"
+
+
+def test_task_isoutdated_false(task_factory):
+    task = task_factory(title="Test task 1")
+
+    assert task.is_outdated() == False
+
+
+def test_task_isoutdated_true(task_factory):
+    task = task_factory(
+        deadline=datetime.datetime(2023, 10, 12, 0, 0, tzinfo=datetime.timezone.utc)
+    )
+
+    assert task.is_outdated() == True
